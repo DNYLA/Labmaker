1932c866c87bf6e8b97a0ddfd99f7244b0ed9374
DanA <22224922+DNYLA@users.noreply.github.com>
2022-01-19 17:16:02 +0000

    Reverted Drop-Down

--- libs/ui/src/input/drop-down.tsx
+++ libs/ui/src/input/drop-down.tsx
@@ -1,170 +1,221 @@
-import { InfoTitle } from './info-box';
+import { useEffect, useState } from 'react';
 import styled from 'styled-components';
-import { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCaretDown, faCaretUp } from '@fortawesome/free-solid-svg-icons';
+import React from 'react';
 
-export interface IOnDateTimeChange {
-  (event: React.ChangeEvent<HTMLInputElement>): void;
+export interface Item {
+  value: number | string;
+  label: string;
 }
+
+export interface IOnDropDownChange {
+  (id: number | string): void;
+}
+
 /* eslint-disable-next-line */
-export interface InputRangeProps {
-  value: number;
-  infoMessage?: string | React.ReactNode;
-  message: string;
-  onChange: IOnDateTimeChange;
+export interface DropDownProps {
+  items: Item[];
+  value: string | number;
+  onChange: IOnDropDownChange;
 }
 
-export function InputRange({
-  message,
-  infoMessage,
-  value,
-  onChange,
-}: InputRangeProps) {
+export function DropDown({ items, value, onChange }: DropDownProps) {
+  const [isOpen, setOpen] = useState(false);
+  const [inputValue, setInputValue] = useState('');
+  const [filteredItems, setFilteredItems] = useState(items);
+  const [selected, setSelected] = useState<Item>(items[0]);
+  const inputRef = React.createRef<HTMLInputElement>();
+
+  useEffect(() => {
+    setInputValue(selected.label);
+  }, [selected]);
+
+  useEffect(() => {
+    // console.log(items);
+    // if (!items) return setSelected({ value: 'loading', label: 'loading' });
+    const startItem = items.find((item) => item.value === value);
+    if (!startItem) return; //Throw Error Instead?
+    setSelected(startItem);
+    setInputValue(startItem.label);
+  }, [items]);
+
+  const setItem = (id: number | string) => {
+    onChange(id);
+    console.log('Running');
+    const newItem = items.find((item) => item.value === id);
+    if (!newItem) return;
+    inputRef.current?.blur();
+    setSelected(newItem);
+    // setInputValue(newItem.label);
+    setOpen(false);
+  };
+
+  const onKeyInput = (inputVal: string) => {
+    setOpen(true);
+    const alreadyInside = items.find(
+      (item) => item.label.toLowerCase() === inputVal
+    );
+    if (alreadyInside && selected.value === alreadyInside.value) {
+      console.log('Already inside');
+      setFilteredItems(items);
+      setInputValue(inputVal);
+      return;
+    }
+    const fItems = items.filter((item) =>
+      item.label.toLowerCase().includes(inputVal)
+    );
+    setFilteredItems(fItems);
+    setInputValue(inputVal);
+  };
+
+  const updateOpen = () => {
+    setFilteredItems(items);
+    if (isOpen && selected.label !== inputValue) {
+      setInputValue(selected.label);
+    }
+    setOpen(!isOpen);
+  };
+
+  const handleInputClick = () => {
+    setInputValue('');
+    setFilteredItems(items);
+    setOpen(true);
+  };
+
+  // const handleCloseClick = () => {
+  //   if (isOpen) {
+  //     setInputValue(selected.label);
+  //     setOpen(false);
+  //   } else {
+  //     setInputValue('');
+  //     setOpen(true);
+  //   }
+  //   setFilteredItems(items);
+  // };
+
+  const handleClose = () => {
+    setInputValue(selected.label);
+    setOpen(false);
+  };
+
   return (
-    <>
-      <InfoTitle title={message} infoMessage={infoMessage} />
-      <StyledInputBox>
-        <StyledInputRange
-          type="datetime-local"
-          value={value}
-          onChange={(e) => onChange(e)}
+    <StyledDropDown
+      tabIndex={0}
+      // onMouseDown={handleCloseClick} //Bug Where Clicking an item closes menu
+      onFocus={handleInputClick}
+      onBlur={handleClose}
+      className={isOpen ? 'open' : ''}
+    >
+      <SelectedItem>
+        <StyledInput
+          value={inputValue}
+          ref={inputRef}
+          onChange={(e) => onKeyInput(e.target.value.toLowerCase())}
         />
-      </StyledInputBox>
-    </>
+
+        <FontAwesomeIcon
+          pull={'right'}
+          icon={isOpen ? faCaretUp : faCaretDown}
+          size="1x"
+          color="#FFF"
+          onClick={updateOpen}
+        />
+      </SelectedItem>
+
+      {isOpen && (
+        <HiddenContainer>
+          {filteredItems.map((item) => {
+            if (item.value !== selected.value)
+              return (
+                <DropDownItem
+                  onMouseDown={(e) => e.preventDefault()}
+                  onClick={() => setItem(item.value)}
+                  key={item.value}
+                >
+                  {item.label}
+                </DropDownItem>
+              );
+            return <div key={item.value}></div>;
+          })}
+        </HiddenContainer>
+      )}
+    </StyledDropDown>
   );
 }
 
-const StyledInputBox = styled.div`
-  user-select: none;
-  display: flex;
-  flex-direction: row;
-  justify-content: space-between;
-  input {
-    /* width: 93%; */
-  }
-  h1 {
-    padding-left: 10px;
-    /* width: 7%; */
+const StyledInput = styled.input`
+  padding-left: 10px;
+  padding-right: 25px;
+  color: white;
+  font-family: 'Archivo Black';
+  width: 100%;
+  height: 30px;
+  background: #1a1a1d;
+  border-radius: 5px;
+  border: 2px solid #141617;
+  font-size: 16px;
+  transition: opacity 340ms ease;
+
+  :focus {
+    opacity: 80%;
+    outline: 0;
   }
 `;
 
-// excess height to improve interactive area / accessibility
-const height = '36px';
-const thumbHeight = 36;
-const trackHeight = '16px';
+const HiddenContainer = styled.div`
+  z-index: 100;
+  position: absolute;
+  width: 196px;
+  border: 2px solid transparent;
+  border-top: 0;
+`;
 
-// colours
+const DropDownItem = styled.div`
+  padding: 10px;
+  z-index: 10;
+  background-color: #1a1a1d;
+  font-family: 'Roboto';
+  font-size: 16px;
 
-const makeLongShadow = (color: any, size: any) => {
-  let i = 18;
-  let shadow = `${i}px 0 0 ${size} ${color}`;
-
-  for (; i < 706; i++) {
-    shadow = `${shadow}, ${i}px 0 0 ${size} ${color}`;
+  :hover {
+    cursor: pointer;
+    background-color: #141617;
   }
+`;
 
-  return shadow;
-};
+const StyledDropDown = styled.div`
+  position: relative;
+  width: 200px;
+  font-size: 18px;
+  transition: all 200ms ease-in;
+  user-select: none;
 
-const StyledInputRange = styled.input`
-  /* width: 100%; */
-  overflow: hidden;
-  display: block;
-  appearance: none;
-  /* max-width: 700px; */
-  width: 100%;
-  margin: 0;
-  height: ${height};
-  background: #202225;
-
-  cursor: pointer;
-
-  &:focus {
-    outline: none;
-  }
-
-  &::-webkit-slider-runnable-track {
-    width: 100%;
-    height: ${height};
-    background: ${(p) => p.theme.input.range.lowerBackground};
-  }
-
-  &::-webkit-slider-thumb {
-    position: relative;
-    appearance: none;
-    height: ${thumbHeight}px;
-    width: ${thumbHeight}px;
-    background: ${(p) => p.theme.input.range.thumbCol};
-    border-radius: 100%;
-    border: 0;
-    top: 50%;
-    transform: translateY(-50%);
-    box-shadow: ${(p) => makeLongShadow(p.theme.input.range.upperCol, '-10px')};
-    transition: background-color 150ms;
-  }
-
-  &::-moz-range-track,
-  &::-moz-range-progress {
-    width: 100%;
-    height: ${height};
-    background: ${(p) => p.theme.input.range.upperBackground};
-  }
-
-  &::-moz-range-progress {
-    background: ${(p) => p.theme.input.range.lowerBackground};
-  }
-
-  &::-moz-range-thumb {
-    appearance: none;
-    margin: 0;
-    height: ${thumbHeight};
-    width: ${thumbHeight};
-    background: ${(p) => p.theme.input.range.thumbCol};
-    border-radius: 100%;
-    border: 0;
-    transition: background-color 150ms;
-  }
-
-  &::-ms-track {
-    width: 100%;
-    height: ${height};
-    border: 0;
-    /* color needed to hide track marks */
-    color: transparent;
-    background: transparent;
-  }
-
-  &::-ms-fill-lower {
-    background: ${(p) => p.theme.input.range.lowerBackground};
-  }
-
-  &::-ms-fill-upper {
-    background: ${(p) => p.theme.input.range.upperBackground};
-  }
-
-  &::-ms-thumb {
-    appearance: none;
-    height: ${thumbHeight};
-    width: ${thumbHeight};
-    background: ${(p) => p.theme.input.range.thumbCol};
-    border-radius: 100%;
-    border: 0;
-    transition: background-color 150ms;
-    /* IE Edge thinks it can support -webkit prefixes */
-    top: 0;
-    margin: 0;
-    box-shadow: none;
-  }
-
-  &:hover,
-  &:focus {
-    &::-webkit-slider-thumb {
-      background-color: ${(p) => p.theme.input.range.thumbHover};
+  &.open {
+    ${StyledInput} {
+      border-bottom: 0;
+      border-radius: 5px 5px 0 0;
     }
-    &::-moz-range-thumb {
-      background-color: ${(p) => p.theme.input.range.thumbHover};
+
+    ${HiddenContainer} {
+      border-color: #141617;
+      border-radius: 0 0 5px 5px;
     }
-    &::-ms-thumb {
-      background-color: ${(p) => p.theme.input.range.thumbHover};
-    }
+  }
+`;
+
+const SelectedItem = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: relative;
+
+  div {
+    color: pink;
+  }
+
+  svg {
+    position: absolute;
+    right: 10px;
   }
 `;
