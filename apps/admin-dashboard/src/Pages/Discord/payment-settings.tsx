37089c5c66dad56f76c69de4e51f3379dbada763
IRHM <37304121+IRHM@users.noreply.github.com>
2022-02-02 00:25:10 +0000

    Use Button component in payment settings

--- apps/admin-dashboard/src/Pages/Discord/payment-settings.tsx
+++ apps/admin-dashboard/src/Pages/Discord/payment-settings.tsx
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 import { PaymentBox } from './payment';
 import { useGuildLogic } from '../../utils/hooks/useGuildLogic';
 import { useNavigate } from 'react-router-dom';
-import { SettingsContainer } from '@labmaker/ui';
+import { Button, SettingsContainer } from '@labmaker/ui';
 import { Payment } from '@labmaker/shared';
 
 /* eslint-disable-next-line */
@@ -24,9 +24,11 @@ export function PaymentSettings(props: PaymentSettingsProps) {
     if (!config) {
       return <div></div>;
     }
+
     console.log(
       `Guild Config: ${config.id} || PaymentID: ${config.paymentConfigId}`
     );
+
     if (config.id === config.paymentConfigId) {
       return payments.map((payment: Payment, index) => {
         if (!payment.deletedPayment) {
@@ -46,11 +48,11 @@ export function PaymentSettings(props: PaymentSettingsProps) {
     } else {
       return (
         <CenterDiv>
-          <CustomButton
+          <Button
             onClick={() => navigate(`/discord/${config.paymentConfigId}`)}
           >
             Go to Payments Config
-          </CustomButton>
+          </Button>
         </CenterDiv>
       );
     }
@@ -66,9 +68,7 @@ export function PaymentSettings(props: PaymentSettingsProps) {
         <PaymentsContainer>{renderPayments(payments)}</PaymentsContainer>
 
         {config.id === config.paymentConfigId && (
-          <CenterDiv>
-            <CustomButton onClick={createPayment}>Add</CustomButton>
-          </CenterDiv>
+          <Button onClick={createPayment}>Create New</Button>
         )}
       </SettingsContainer>
     );
@@ -78,17 +78,16 @@ export function PaymentSettings(props: PaymentSettingsProps) {
 const CenterDiv = styled.div`
   display: flex;
   justify-content: center;
-  margin-top: 10px;
-
-  button {
-    margin-right: 25px;
-  }
 `;
 
 const PaymentsContainer = styled.div`
   max-height: 50vh;
   overflow-y: auto;
-  padding-right: 20px;
+
+  & > *:not(:last-child) {
+    margin-bottom: 15px;
+  }
+
   /* width */
   ::-webkit-scrollbar {
     width: 10px;
@@ -114,29 +113,5 @@ const PaymentsContainer = styled.div`
 
   @media (max-width: 800px) {
     max-height: unset;
-  }
-`;
-
-const CustomButton = styled.button`
-  width: 50%;
-  height: 28px;
-  border: none;
-  background-color: #313c4b;
-  border-radius: 5px;
-  justify-content: center;
-  color: white;
-  font-family: 'Roboto';
-  font-size: 18px;
-  outline: none;
-  transition: 0.5s;
-
-  :active {
-    border: none;
-  }
-
-  :hover {
-    background-color: #455366;
-    transition: 0.5s;
-    cursor: pointer;
   }
 `;
