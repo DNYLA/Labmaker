efc05c563d667be4f6001ce9076dd5387943a1af
Dan <22224922+DNYLA@users.noreply.github.com>
2022-02-04 09:49:37 +0000

    Updated User Schema

---
+++ prisma/migrations/20220204093615_added_apply_date/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "User" ADD COLUMN     "nextApply" TIMESTAMP(3);
---
+++ prisma/migrations/20220204093738_role_not_optional/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - Made the column `role` on table `User` required. This step will fail if there are existing NULL values in that column.
+
+*/
+-- AlterTable
+ALTER TABLE "User" ALTER COLUMN "role" SET NOT NULL;
