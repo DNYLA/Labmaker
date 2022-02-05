11a44085778eb93174437e06fa05eacf7e8fd630
DanA <22224922+DNYLA@users.noreply.github.com>
2022-02-05 13:54:09 +0000

    Added Helmet to Prevents XSS + Other Attacks

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -5,22 +5,22 @@ import * as passport from 'passport';
 import * as cookieParser from 'cookie-parser';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import * as compression from 'compression';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
   app.enable('trust proxy');
-
-  app.use(cookieParser());
   app.enableCors({
     origin: [process.env.ADMIN_DASH_URL, process.env.USER_DASH_URL],
     credentials: true,
   });
-  app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
 
+  app.use(helmet()); //HELMET MUST COME FIRST
+  app.use(cookieParser());
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
   app.use(passport.initialize());
   app.use(compression());
-  // app.useWebSocketAdapter(new IoAdapter(app));
 
   await app.listen(process.env.PORT || 3000);
   Logger.log(
